c7b30dae00e3fa245399949e3a16f163c661e52b
kestr31 <98739766+kestr31@users.noreply.github.com>
2023-11-05 19:26:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# CONTAINERIZED GAZEBO CLASSIC CONTAINER FOR PX4 SITL
+
+## 0. OVERVIEW
+
+- Further documentation will be done.
+
+## 1. AVAIABLE TAGS & BUILD ORDERS
+
+- TBD
+
+## 2. ENVIRONMENT VARIABLE SETUPS
+
+- TBD
+
+
+## 3. HOW-TO-BUILD
+
+```shell
+DOCKER_BUILDKIT=1 docker build --no-cache \
+--build-arg BASEIMAGE=ubuntu \
+--build-arg BASETAG=20.04 \
+-t kestr3l/gazebo:classic-11 \
+-f ./Dockerfile .
+```
+
+## 4. ATTACHING CONTAINER TO SITL
+
+- TBD
---
+++ entrypoint.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+
+debug_message() {
+    echo "
+        ____  __________  __  ________   __  _______  ____  ______
+       / __ \/ ____/ __ )/ / / / ____/  /  |/  / __ \/ __ \/ ____/
+      / / / / __/ / __  / / / / / __   / /|_/ / / / / / / / __/   
+     / /_/ / /___/ /_/ / /_/ / /_/ /  / /  / / /_/ / /_/ / /___   
+    /_____/_____/_____/\____/\____/  /_/  /_/\____/_____/_____/   
+    
+    "
+    echo "INFO [SITL] DEBUG_MODE IS SET. NOTHING WILL RUN"
+}
+
+
+PX4_SOURCE_DIR=/home/user/PX4-Autopilot
+PX4_SIM_DIR=/home/user/PX4-Autopilot/Tools/simulation
+PX4_BUILD_DIR=/home/user/PX4-Autopilot/build/px4_sitl_default
+PX4_BINARY_DIR=/home/user/PX4-Autopilot/build/px4_sitl_default/bin
+
+# DIRECTORY TO PX4 gazebo SITL WORLD/MODEL OBJECTS
+PX4_GZ_WORLDS=${PX4_SIM_DIR}/gazebo-classic/sitl_gazebo-classic/worlds
+PX4_GZ_MODELS=${PX4_SIM_DIR}/gazebo-classic/sitl_gazebo-classic/models
+
+# SET GAZEBO RESOURCE PATH
+# export GAZEBO_MODEL_PATH=${PX4_GZ_MODELS}:${GAZEBO_USER_MODEL_PATH}
+source /usr/share/gazebo/setup.bash
+
+export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:${PX4_GZ_WORLDS}:${GAZEBO_USER_RESOURCE_PATH}
+
+# PREVENT GAZEBO FROM RUNNING WHEN RUNNING PX4 STACK
+COMMENT_START=$(grep -wn "sitl_command=" ${PX4_SIM_DIR}/gazebo-classic/sitl_run.sh | cut -d: -f1)
+COMMENT_END=$(grep -wn "popd >/dev/null" ${PX4_SIM_DIR}/gazebo-classic/sitl_run.sh | cut -d: -f1)
+
+COMMENT_END=$(echo ${COMMENT_END} | tail -1)
+COMMENT_END=$((${COMMENT_END} + 6))
+
+sed -i "${COMMENT_START},${COMMENT_END}s/\(.*\)/#\1/g" \
+    ${PX4_SIM_DIR}/gazebo-classic/sitl_run.sh
+
+sed -i "s/-n \"\$HEADLESS\"/\$HEADLESS -eq 1/g" \
+    ${PX4_SIM_DIR}/gazebo-classic/sitl_run.sh
+
+
+# A. DEBUG MODE / SIMULATION SELCTOR
+## CASE A-1: DEBUG MODE
+if [ "${DEBUG_MODE}" -eq "1" ]; then
+
+    debug_message
+
+    ## A-1. EXPORT ENVIRONMENT VARIABLE?
+    ### CASE A-1-1: YES EXPORT THEM
+    if [ "${EXPORT_ENV}" -eq "1" ]; then
+
+        #- GET LINE NUMBER TO START ADDING export STATEMENT
+        COMMENT_BASH_START=$(grep -c "" /home/user/.bashrc)
+        COMMENT_ZSH_START=$(grep -c "" /home/user/.zshrc)
+
+        COMMENT_BASH_START=$(($COMMENT_BASH_START + 1))
+        COMMENT_ZSH_START=$(($COMMENT_ZSH_START + 1))
+
+
+        #- WTIE VARIABLED TO BE EXPORTED TO THE TEMPFILE
+        echo "DEBUG_MODE=0" >> /tmp/envvar
+        echo "GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}" >> /tmp/envvar
+
+        #- ADD VARIABLES TO BE EXPORTED TO SHELL RC
+        for value in $(cat /tmp/envvar)
+        do
+            echo ${value} >> /home/user/.bashrc
+            echo ${value} >> /home/user/.zshrc
+        done
+
+        #- ADD export STATEMENT TO VARIABLES
+        sed -i "${COMMENT_BASH_START},\$s/\(.*\)/export \1/g" \
+            ${HOME}/.bashrc
+        sed -i "${COMMENT_ZSH_START},\$s/\(.*\)/export \1/g" \
+            ${HOME}/.zshrc
+
+        #- REMOVE TEMPORARY FILE
+        rm -f /tmp/envvar
+
+    ### CASE A-1-2: NO LEAVE THEM CLEAN
+    else
+        echo "INFO [SITL] ENVIRONMENT VARS WILL NOT BE SET"
+    fi
+
+
+## CASE A-2: SIMULATION MODE
+else
+
+    echo "INFO [SIM] RUNNING gazebo SIMULATOR"
+    
+    ${PX4_SIM_DIR}/gazebo-classic/sitl_run.sh \
+        ${PX4_BINARY_DIR}/px4 \
+        none \
+        ${SITL_AIRFRAME} \
+        ${SITL_WORLD} \
+        ${PX4_SOURCE_DIR} \
+        ${PX4_BUILD_DIR}
+
+fi
+
+# KEEP CONTAINER ALIVE
+sleep infinity
